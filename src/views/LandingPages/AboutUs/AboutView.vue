1d72e40ee85fa2e987566cdf2337167383e300d1
ferranm99 <ferranm99@gmail.com>
2023-03-25 18:50:21 +0400

    price caroussel

--- src/views/LandingPages/AboutUs/AboutView.vue
+++ src/views/LandingPages/AboutUs/AboutView.vue
@@ -15,9 +15,8 @@ import Typed from "typed.js";
 
 //sections
 import Information from "./Sections/AboutInformation.vue";
-import AboutTeam from "./Sections/AboutTeam.vue";
-import Featuring from "./Sections/AboutFeaturing.vue";
-import Newsletter from "./Sections/AboutNewsletter.vue";
+import Video from "./Sections/Video.vue";
+import Prices from "./Sections/Prices.vue";
 
 const body = document.getElementsByTagName("body")[0];
 //hooks
@@ -86,9 +85,8 @@ onUnmounted(() => {
   </header>
   <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
     <Information />
-    <AboutTeam />
-    <Featuring />
-    <Newsletter />
+    <Video />
+    <Prices />
   </div>
   <DefaultFooter />
 </template>
---
+++ src/views/LandingPages/AboutUs/Sections/Prices.vue
@@ -0,0 +1,79 @@
+<script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel'
+import CenteredBlogCard from '../../../../examples/cards/blogCards/CenteredBlogCard.vue';
+
+const slides = [
+  { id: '1', title: 'Vue 3 Introduction', content: 'VueJS is a library' },
+  { id: '2', title: 'Vue 3 Components', content: 'Know the components' },
+  { id: '3', title: 'Vue 3 Conditional', content: 'Rendering Conditionally' },
+  { id: '4', title: 'Vue 3 Reactivity', content: 'VueJS is Reactive' },
+  { id: '5', title: 'Vue 3 Compute', content: 'VueJS uses computed properties' },
+]
+</script>
+
+<!--
+<template>
+  <section class="py-7">
+    <div class="container">
+      <Carousel :itemsToShow="3.95" :wrapAround="true" :transition="500">
+          <Slide v-for="slide in 5" :key="slide">
+            <CenteredBlogCard
+              image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+              title="Get insights on Search"
+              description="Website visitors today demand a frictionless user expericence — especially when using search. Because of the hight standards." />
+          </Slide>
+        </Carousel>
+    </div>
+  </section>
+</template>
+-->
+
+
+<template>
+  <section class="py-3">
+    <div class="container">
+      <Carousel>
+        <Slide v-for="slide in slides" :key="slide.id">
+          <div class="carousel__item">
+            <div class="col-lg-4 ms-auto me-auto mt-lg-0 mt-6">
+              <CenteredBlogCard
+                image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                :title=slide.title :description=slide.content />
+            </div>
+          </div>
+        </Slide>
+
+        <template #addons>
+          <Navigation />
+          <Pagination />
+        </template>
+      </Carousel>
+    </div>
+  </section>
+</template>
+
+<style>
+.carousel__item {
+  margin-top: 5%;
+  min-height: 200px;
+  width: 100%;
+  font-size: 20px;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 1px solid lightgrey;
+  background-color: #85FFBD;
+  background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
+  border-radius: 20px;
+}
+</style>
